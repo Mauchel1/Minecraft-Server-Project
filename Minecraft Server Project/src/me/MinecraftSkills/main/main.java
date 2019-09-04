f49afc2fad39078006d48b2c22cc41b382418a23
Friedrich, Daniel <Daniel.Friedrich@hbg.yxlon.com>
2019-09-04 15:33:05 +0200

    Erste Testversion funktionsfähig

--- Minecraft Server Project/src/me/MinecraftSkills/main/main.java
+++ Minecraft Server Project/src/me/MinecraftSkills/main/main.java
@@ -1,11 +1,10 @@
 package me.MinecraftSkills.main;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import net.md_5.bungee.api.ChatColor;
 
 public class main extends JavaPlugin{
 
