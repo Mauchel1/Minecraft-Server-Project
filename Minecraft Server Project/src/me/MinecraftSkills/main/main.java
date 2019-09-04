6392623e6aa1f2b21e1dc4e9a649b42b3c3a3af7
Friedrich, Daniel <Daniel.Friedrich@hbg.yxlon.com>
2019-09-04 14:36:07 +0200

    Initial Commit

---
+++ Minecraft Server Project/src/me/MinecraftSkills/main/main.java
@@ -0,0 +1,30 @@
+package me.MinecraftSkills.main;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import net.md_5.bungee.api.ChatColor;
+
+public class main extends JavaPlugin{
+
+	// Fired when plugin is first enabled
+    @Override
+    public void onEnable() {
+    	Bukkit.getConsoleSender().sendMessage(ChatColor.RED+"TestText");
+    	System.out.println("Plugin MinecraftSkills gestartet");
+    }
+    
+    // Fired when plugin is disabled
+    @Override
+    public void onDisable() {
+    	System.out.println("Plugin MinecraftSkills gestoppt");
+    }
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
+    {
+    	return false;
+    }
+}
---
+++ Minecraft Server/start.bat
@@ -0,0 +1,2 @@
+java -Xms512M -Xmx1G -jar spigot-1.14.4.jar -o true
+@pause
