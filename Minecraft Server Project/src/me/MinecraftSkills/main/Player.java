f3435a692d7982b5bb254a014f50bdfc3b1053a3
Friedrich, Daniel <Daniel.Friedrich@hbg.yxlon.com>
2019-09-06 09:53:25 +0200

    create, load and save player config file (yml)

---
+++ Minecraft Server Project/src/me/MinecraftSkills/main/Player.java
@@ -0,0 +1,26 @@
+package me.MinecraftSkills.main;
+
+public class Player {
+
+	String uuid;
+	
+	int lvlWoodcutting;
+	int lvlAgility;
+	int lvlMining;
+	int lvlFarming;
+	int lvlAlchemy;
+	int lvlFishing;
+	
+	int xpWoodcutting;
+	int xpAgility;
+	int xplMining;
+	int xpFarming;
+	int xpAlchemy;
+	int xpFishing;
+	
+	
+	public Player(String UUID) {
+		uuid = UUID;
+	}
+	
+}
--- Minecraft Server Project/src/me/MinecraftSkills/main/main.java
+++ Minecraft Server Project/src/me/MinecraftSkills/main/main.java
@@ -1,29 +1,65 @@
 package me.MinecraftSkills.main;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class main extends JavaPlugin{
 
+    private File playerConfigFile;
+    private FileConfiguration playerConfig;
+    
+    private List<Player> playerList;
+
+	
 	// Fired when plugin is first enabled
     @Override
     public void onEnable() {
-    	Bukkit.getConsoleSender().sendMessage(ChatColor.RED+"TestText");
+    	Bukkit.getConsoleSender().sendMessage(ChatColor.RED+"Plugin MinecraftSkills gestartet");
     	System.out.println("Plugin MinecraftSkills gestartet");
+    	loadConfig();
     }
     
     // Fired when plugin is disabled
     @Override
     public void onDisable() {
+    	try {
+			playerConfig.save(playerConfigFile);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
     	System.out.println("Plugin MinecraftSkills gestoppt");
+    	Bukkit.getConsoleSender().sendMessage(ChatColor.RED+"Plugin MinecraftSkills gestoppt");
     }
     
-    @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
-    {
-    	return false;
+    private void loadConfig() {
+        playerConfigFile = new File(getDataFolder(), "playerconfig.yml");
+        if (!playerConfigFile.exists()) {
+            playerConfigFile.getParentFile().mkdirs();
+            saveResource("playerconfig.yml", false);
+         }
+
+        playerConfig= new YamlConfiguration();
+        try {
+            playerConfig.load(playerConfigFile);
+        } catch (IOException | InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
     }
+    
+    public FileConfiguration getPlayerConfig() {
+        return this.playerConfig;
+    }
+//    @Override
+//    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
+//    {
+//    	return false;
+//    }
 }
