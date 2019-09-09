f232ad7d197b137d47449e3b1045f2fbd603c518
Daniel Friedrich <DanielDanielfriedrich@gmail.com>
2019-09-10 00:16:08 +0200

    xp berechnung dokumentiert

--- Minecraft Server Project/src/me/MinecraftSkills/main/Player.java
+++ Minecraft Server Project/src/me/MinecraftSkills/main/Player.java
@@ -23,4 +23,17 @@ public class Player {
 		uuid = UUID;
 	}
 	
+	
+//	XP-Berechnung
+//	x = 10(n^2-m^2) + 1010(n-m)
+//	or
+//	10(n-m)(n+m+101)
+//	where n is the target level and m is the current level.
+//
+//	To find how much exp is needed to get to level "L" from 0 exp, this equation can be used:
+//	exp = 1010L + 10L^2
+//	or
+//	10L(101+L)
+
+
 }
