edcebaee25c3cd8265d44cd6bcd9b4120fbc03d5
Friedrich, Daniel <Daniel.Friedrich@hbg.yxlon.com>
2019-10-08 12:47:55 +0200

    update Spigot API

--- Minecraft Server Project/src/me/MinecraftSkills/main/main.java
+++ Minecraft Server Project/src/me/MinecraftSkills/main/main.java
@@ -3,7 +3,6 @@ package me.MinecraftSkills.main;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import me.MinecraftSkills.main.Commands.*;
